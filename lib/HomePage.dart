acd8f8ca2ab6e6c1f04a73b19cb9e1125fce5760
Tojann204 <tojan050@gmail.com>
2024-07-07 23:00:06 +0300

    th

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -13,15 +13,15 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('My Items')),
-      body: ListView.builder(
-        itemBuilder: (context,index)=>ListTile(
-          title: Text(groceryItems[index].name),
-          leading: Container(height: 24,width: 24,color:groceryItems[index].category.color ,),
-          trailing: Text(groceryItems[index].quantity.toString()),
+      // body: ListView.builder(
+      //   itemBuilder: (context,index)=>ListTile(
+      //     title: Text(groceryItems[index].name),
+      //     leading: Container(height: 24,width: 24,color:groceryItems[index].category.color ,),
+      //     trailing: Text(groceryItems[index].quantity.toString()),
 
 
-        )
-        ),
+      //   )
+        
     );
   }
 }
